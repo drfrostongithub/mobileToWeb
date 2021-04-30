f1928efc7696f2c06a627d02a7883adabe20164c
Putra Reza Fardani <devtcp.pfardani@gmail.com>
2021-04-30 09:21:01 +0700

    header + footer fix

--- src/components/_jsapp/Header.js
+++ src/components/_jsapp/Header.js
@@ -1,21 +1,20 @@
 import * as React from "react";
-import icon from '../img/iconhead.png'
+import icon from "../img/iconhead.png";
 
 export default function Header() {
   return (
     <header>
       <div className='title'>
         <a href='/' className='status'>
-          <img src={icon}
-            alt='img'
-            className='iconHead'
-          />
-          <p>
-            <strong>RemoteFlow</strong>
-          </p>
+          <img src={icon} alt='img' className='iconHead' />
+          <div className='greetings'>
+            <p>Good Morning</p>
+            <p>
+              <strong>Fellas</strong>
+            </p>
+          </div>
         </a>
       </div>
-
     </header>
   );
 }
