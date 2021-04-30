61b87cfce18bff92ede3800e2ddf2c3ab004fa59
Putra Reza Fardani <devtcp.pfardani@gmail.com>
2021-04-30 10:06:35 +0700

    body improved

--- src/components/_jsapp/Body.js
+++ src/components/_jsapp/Body.js
@@ -1,16 +1,11 @@
 import * as React from 'react'
-// import { ReactComponent as BG } from './../img/modal1.svg'
-// import BG from './../img/modal1.svg'
+
 const Body = () => {
 
     return (
         <main>
-            {/* <img src={BG}
-                alt="modal1"
-                className='background'
-            /> */}
             <div className="background">
-                Here
+                
             </div>
         </main>
     )
