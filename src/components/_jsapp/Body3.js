c44bf6a44eb041eaa033b7be3bd174f39e0cffa9
drfrostongithub <eternal.putra@gmail.com>
2021-05-02 19:23:09 +0700

    loop ?

--- src/components/_jsapp/Body3.js
+++ src/components/_jsapp/Body3.js
@@ -1,9 +1,47 @@
-import * as React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Testimonials from './Testimonials'
+
 const Body3 = () => {
 
+    const [testimonial, setTestimonial] = useState([])
+    const testi = []
+    useEffect(() => {
+        const fetchUsers = async () => {
+            await axios
+                .get('https://wknd-take-home-challenge-api.herokuapp.com/testimonial')
+                .then(res => {
+                    const item = JSON.stringify(res.data)
+                    setTestimonial(item)
+                    // console.log(res)
+                })
+                .catch(err => {
+                    console.error(err)
+                })
+
+        }
+        fetchUsers()
+
+    }, []);
+
+    if (testimonial.length > 0) {
+        testimonial.forEach((item, i) => {
+            console.log(item)
+            // testi.push(<Testimonials
+            //     key={'testimonial-' + i}
+            //     item={item} />)
+        });
+    }
     return (
         <div className='body3'>
-            Body 3 Here
+            <div>
+                <label>
+                    <h1>
+                        Testimonial
+                    </h1>
+                    {testimonial}
+                </label>
+            </div>
         </div>
     )
 }
